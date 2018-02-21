37eac59f9d050f4ae1e031dc610dce87ef127739
EC2 Default User <ec2-user@csad1.localdomain>
2018-02-21 22:47:41 +0000

    new t2unlimitted cookbook

--- t2unlimitted/recipes/default.rb
+++ t2unlimitted/recipes/default.rb
@@ -16,11 +16,21 @@ template '/home/ec2-user/t2.unlimitted.sh' do
   owner 'root'
   group 'root'
   mode '0755'
-  variables(
+  variables({
     :location => node['t2unlimitted']['status']
-  )
+  })
 end
 
 execute 'configure_t2unlimitted' do
   command '/bin/bash /home/ec2-user/t2.unlimitted.sh'
+end
+
+ruby_block "Results" do
+    only_if { ::File.exists?(results) }
+    block do
+        print "\n"
+        File.open(results).each do |line|
+            print line
+        end
+    end
 end
