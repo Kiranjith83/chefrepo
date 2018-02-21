aa8f50ce0a4922baaf6ee5edd07fb52733685ff2
EC2 Default User <ec2-user@csad1.localdomain>
2018-02-21 22:22:27 +0000

    new t2unlimitted cookbook

--- t2unlimitted/recipes/default.rb
+++ t2unlimitted/recipes/default.rb
@@ -5,17 +5,13 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
 
-bash 'setup_t2.unlimitted' do
-  code <<-EOH
-          sudo yum update aws-cli -y
-          INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
-          REGION=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//'`
-          SPECIFICATIONS_JSON="[{\"InstanceId\": \"${INSTANCE_ID}\",\"CpuCredits\": \"unlimited\"}]"
-          echo "INSTANCE_ID: ${INSTANCE_ID}"
-          echo "REGION: ${REGION}"
-          echo "SPECIFICATIONS_JSON: ${SPECIFICATIONS_JSON}"
-          aws --region ${REGION} ec2 describe-instance-credit-specifications --instance-id ${INSTANCE_ID}
-          aws --region ${REGION} ec2 modify-instance-credit-specification --instance-credit-specification "${SPECIFICATIONS_JSON}"
-          aws --region ${REGION} ec2 describe-instance-credit-specifications --instance-id ${INSTANCE_ID}
-          EOH
+template '/home/ec2-user/t2.unlimitted.sh' do
+  source 't2unlimitted.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+execute 'configure_t2unlimitted' do
+  command '/bin/bash /home/ec2-user/t2.unlimitted.sh'
 end
