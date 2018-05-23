b0ac4c3b4b7d2520643ac3a93741fa8129a51e97
kiranjith83@gmail.com <kiranjith83@gmail.com>
2018-05-23 11:18:14 +1000

    Added webserver installation

--- webserver/recipes/default.rb
+++ webserver/recipes/default.rb
@@ -5,12 +5,15 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
 
-package 'httpd' do
-  case node[:platform]
-  when 'redhat', 'centos'
-    package_name 'httpd'
-  when 'ubuntu', 'debian'
-    package_name 'apache2'
-  end
+package 'httpd'
+
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  mode '0755'
+  owner 'root'
 end
 
+service "httpd" do
+  action :restart
+end
