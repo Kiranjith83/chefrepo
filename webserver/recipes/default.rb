c91f4175e957d9d9f92b97ea7c1b01667459aa60
kiranjith83@gmail.com <kiranjith83@gmail.com>
2018-05-23 11:40:28 +1000

    Added webserver installation

--- webserver/recipes/default.rb
+++ webserver/recipes/default.rb
@@ -7,11 +7,14 @@
 
 package 'httpd'
 
-
 template '/var/www/html/index.html' do
   source 'index.html.erb'
   mode '0755'
   owner 'root'
+  variables(
+   :motd => "this is the message mate!",
+   :myenvars => "#{deploy[:environment_variables]}"
+)
 end
 
 service "httpd" do
