b2589e00b884ef4d3ed3f6f8f067b642e1d6b3db
kiranjith83@gmail.com <kiranjith83@gmail.com>
2018-05-23 10:59:58 +1000

    Added webserver installation

--- webserver/recipes/default.rb
+++ webserver/recipes/default.rb
@@ -5,4 +5,12 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
 
-package 'Badhttpd'
+package 'Install Apache' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
+end
+
